30be9b811a15c7058422fe21eb0f5057b5143581
Peter Novotnak <pnovotnak@flexion.us>
2014-09-14 15:59:41 -0700

    settings class complete, aside from comments being lost when the config is updated from the default

--- TTYAnthem.py
+++ TTYAnthem.py
@@ -1,20 +1,46 @@
 #!/usr/bin/env python
 
-import argparse
+'''
+The main interface to TTYAnthem.
+
+Loads the app if it's called directly, otherwise it doesn't do anything.
+'''
+
+from TTYAnthem.config import Config
+#import argparse
 
 
 class Loader(object):
+    '''
+    Loads the app
+    '''
 
     def __init__(self):
-
+        '''
+        Initialize the app
+        '''
         # Get command line arguments
-        parser = argparse.ArgumentParser()
-        parser.parse_args()
+        #parser = argparse.ArgumentParser()
+        #parser.parse_args()
+
+        self.settings = Config()
+
+    def shutdown(self):
+        '''
+        Shut down the app
+        '''
+
+    def daemonize(self):
+        '''
+        Daemonize app
+        '''
+
 
 
 if __name__ == "__main__":
-    l = None
+    L = None
     try:
-        l = Loader()
+        L = Loader()
+
     except:
         raise
--- TTYAnthem/config.py
+++ TTYAnthem/config.py
@@ -3,6 +3,19 @@ import ConfigParser
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+DEFAULT_SETTINGS = {
+    'GUI': {
+        '#': 'GUI configuration section.',
+
+        'type': 'web',
+        'listen': '127.0.0.1',
+    },
+
+    'TTYs': {
+        '#': 'TTYs configuration. Multiple TTYs may be configured here'
+    }
+}
+
 
 class Config(object):
     '''
@@ -10,7 +23,7 @@ class Config(object):
     config file defined above.
     '''
 
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         '''
         Set values, provide an interface to override attributes
         '''
@@ -20,16 +33,43 @@ class Config(object):
 
         # Unpack kwargs
         for key in kwargs:
-            print "another keyword arg: %s: %s" % (key, kwargs[key])
 
-        config = ConfigParser.RawConfigParser()
-        config.read(self.config_file)
+            # Override config file location
+            if key == 'config_file':
+                self.config_file = kwargs[key]
 
-    def get(self, key):
+        self.config = ConfigParser.RawConfigParser(allow_no_value=True)
+        self.config.read(self.config_file)
+
+        # Load settings or write defaults
+        if self.config.sections():
+            pass
+        else:
+            self.defaults()
+
+    def get(self, section, key):
         '''
-        Get a config value
+        Get a single config value
+
+        Just an alias to ConfigParser.get(section, option)
         '''
 
+        return self.config.get(section, key)
+
+    def set(self, section, key, value):
+        '''
+        Set a single config value
+        '''
+
+        #
+        self.config.set(section, key, value)
+
+        # Write out the default settings
+        with open(self.config_file, 'wb') as configfile:
+            self.config.write(configfile)
+
+        # Reload settings file
+        self.config.read(self.config_file)
 
     def defaults(self):
         '''
@@ -37,3 +77,24 @@ class Config(object):
         set default values
         '''
 
+        config = ConfigParser.RawConfigParser(allow_no_value=True)
+
+        # Parse DEFAULT_SETTINGS dict
+        for section in DEFAULT_SETTINGS.keys():
+            config.add_section(section)
+            for setting in DEFAULT_SETTINGS[section]:
+                # Comment lines
+                if setting == '#':
+                    config.set(section, ' '.join([
+                        setting,
+                        DEFAULT_SETTINGS[section][setting]
+                    ]))
+                else:
+                    config.set(section, setting, DEFAULT_SETTINGS[section][setting])
+
+        # Write out the default settings
+        with open(self.config_file, 'wb') as configfile:
+            config.write(configfile)
+
+        # Reload settings file
+        self.config.read(self.config_file)
